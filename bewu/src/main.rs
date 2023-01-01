53a0ecb08fc272d1eb84966d5ad02b68780464df
adumbidiot <nathaniel.daniel23@outlook.com>
2022-12-31 19:34:23 -0800

    Move routes to new file

--- bewu/src/main.rs
+++ bewu/src/main.rs
@@ -1,19 +1,9 @@
 mod logger;
+mod routes;
 
 use anyhow::Context;
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
-use axum::routing::get_service;
-use axum::Router;
 use std::net::SocketAddr;
 use std::path::Path;
-use tower_http::services::ServeDir;
-use tower_http::services::ServeFile;
-use tower_http::trace::DefaultMakeSpan;
-use tower_http::trace::DefaultOnFailure;
-use tower_http::trace::DefaultOnRequest;
-use tower_http::trace::DefaultOnResponse;
-use tower_http::trace::TraceLayer;
 use tracing::error;
 use tracing::info;
 
@@ -44,25 +34,8 @@ fn main() -> anyhow::Result<()> {
     tokio_rt.block_on(async_main(config))
 }
 
-async fn server_error(_err: std::io::Error) -> impl IntoResponse {
-    (StatusCode::INTERNAL_SERVER_ERROR, "Server Error")
-}
-
 async fn async_main(config: Config) -> anyhow::Result<()> {
-    let static_file_dir =
-        std::fs::canonicalize("public").context("failed to canonicalize static file dir")?;
-
-    let serve_dir =
-        ServeDir::new(static_file_dir).not_found_service(ServeFile::new("public/index.html"));
-    let serve_dir = get_service(serve_dir).handle_error(server_error);
-    let app = Router::new().fallback_service(serve_dir).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().level(tracing::Level::INFO))
-            .on_request(DefaultOnRequest::new().level(tracing::Level::INFO))
-            .on_response(DefaultOnResponse::new().level(tracing::Level::INFO))
-            .on_failure(DefaultOnFailure::new().level(tracing::Level::ERROR)),
-    );
-
+    let app = self::routes::routes()?;
     let server = axum::Server::try_bind(&config.address).context("failed to bind to address")?;
 
     info!("listening on {}", config.address);
