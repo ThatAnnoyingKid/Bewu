93f4a4bc30cc9d310d870147a7c3247be8355e57
adumbidiot <nathaniel.daniel23@outlook.com>
2022-12-31 19:20:55 -0800

    Add kitsu lib

---
+++ lib/kitsu-rs/src/lib.rs
@@ -0,0 +1,77 @@
+mod types;
+
+pub use crate::types::AgeRating;
+pub use crate::types::Anime;
+pub use crate::types::Status;
+pub use crate::types::Subtype;
+pub use json_api::JsonDocument;
+pub use json_api::ResourceObject;
+
+/// The error type
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// A JsonApi error
+    #[error(transparent)]
+    JsonApi(#[from] json_api::Error),
+}
+
+/// The client
+#[derive(Default)]
+pub struct Client {
+    client: json_api::Client,
+}
+
+impl Client {
+    /// Make a new client
+    pub fn new() -> Self {
+        Client {
+            client: json_api::Client::new(),
+        }
+    }
+
+    /// Perform a search for anime
+    pub async fn search(
+        &self,
+        query: &str,
+    ) -> Result<JsonDocument<Vec<ResourceObject<Anime>>>, Error> {
+        let url = format!("https://kitsu.io/api/edge/anime?filter[text]={}", query);
+        Ok(self.client.get_json_document(&url).await?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SEARCHES: &[&str] = &[
+        include_str!("../test_data/3-gatsu_no_Lion_2nd_Season_search.json"),
+        include_str!("../test_data/cowboy_bebop_search.json"),
+        include_str!("../test_data/5_Centimeter_per_Second_search.json"),
+        include_str!("../test_data/food_search.json"),
+        include_str!("../test_data/high_search.json"),
+    ];
+
+    #[test]
+    fn parse_searches() {
+        for search_json in SEARCHES {
+            let search =
+                serde_json::from_str::<JsonDocument<Vec<ResourceObject<Anime>>>>(search_json);
+
+            match search {
+                Ok(_search) => {
+                    // TODO: Consider comparing with "Answer" array.
+                }
+                Err(e) => {
+                    panic!("{:#?}", e);
+                }
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn it_works() {
+        let client = Client::new();
+        let res = client.search("food").await.unwrap();
+        dbg!(res);
+    }
+}
---
+++ lib/kitsu-rs/src/types.rs
@@ -0,0 +1,149 @@
+use std::collections::HashMap;
+
+/// An Anime Object.
+/// [Spec](https://kitsu.docs.apiary.io/#reference/anime)
+#[derive(Debug, serde::Deserialize)]
+pub struct Anime {
+    #[serde(rename = "createdAt")]
+    pub created_at: String,
+
+    #[serde(rename = "updatedAt")]
+    pub updated_at: String,
+
+    pub slug: String,
+    pub synopsis: String,
+    pub titles: HashMap<String, Option<String>>,
+
+    #[serde(rename = "canonicalTitle")]
+    pub canonical_title: String,
+
+    #[serde(rename = "abbreviatedTitles")]
+    pub abbreviated_titles: Option<Vec<String>>,
+
+    #[serde(rename = "averageRating")]
+    pub average_rating: Option<String>,
+
+    #[serde(rename = "ratingFrequencies")]
+    pub rating_frequencies: HashMap<u64, String>,
+
+    #[serde(rename = "userCount")]
+    pub user_count: u64,
+
+    #[serde(rename = "favoritesCount")]
+    pub favorites_count: u64,
+
+    #[serde(rename = "startDate")]
+    pub start_date: Option<String>,
+
+    #[serde(rename = "endDate")]
+    pub end_date: Option<String>,
+
+    #[serde(rename = "popularityRank")]
+    pub popularity_rank: u64,
+
+    #[serde(rename = "ratingRank")]
+    pub rating_rank: Option<u64>,
+
+    #[serde(rename = "ageRating")]
+    pub age_rating: Option<AgeRating>,
+
+    #[serde(rename = "ageRatingGuide")]
+    pub age_rating_guide: String,
+
+    pub subtype: Subtype,
+    pub status: Status,
+    pub tba: Option<String>,
+
+    #[serde(rename = "posterImage")]
+    pub poster_image: serde_json::Value,
+
+    #[serde(rename = "coverImage")]
+    pub cover_image: Option<serde_json::Value>,
+
+    #[serde(rename = "episodeCount")]
+    pub episode_count: Option<u64>,
+
+    #[serde(rename = "episodeLength")]
+    pub episode_length: Option<u64>,
+
+    #[serde(rename = "youtubeVideoId")]
+    pub youtube_video_id: Option<String>,
+
+    pub nsfw: bool,
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(from = "&str")]
+pub enum AgeRating {
+    General,
+    ParentalGuidance,
+    Restricted,
+    Explicit,
+
+    Other(String),
+}
+
+impl<'a> From<&'a str> for AgeRating {
+    fn from(s: &str) -> Self {
+        match s {
+            "G" => Self::General,
+            "PG" => Self::ParentalGuidance,
+            "R" => Self::Restricted,
+            "R18" => Self::Explicit,
+            _ => Self::Other(s.into()),
+        }
+    }
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(from = "&str")]
+pub enum Subtype {
+    Ona,
+    Ova,
+    Tv,
+    Movie,
+    Music,
+    Special,
+
+    Other(String),
+}
+
+impl<'a> From<&'a str> for Subtype {
+    fn from(s: &'a str) -> Self {
+        match s {
+            "ONA" => Self::Ona,
+            "OVA" => Self::Ova,
+            "TV" => Self::Tv,
+            "movie" => Self::Movie,
+            "music" => Self::Music,
+            "special" => Self::Special,
+
+            _ => Self::Other(s.into()),
+        }
+    }
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(from = "&str")]
+pub enum Status {
+    Current,
+    Finished,
+    Tba,
+    Unreleased,
+    Upcoming,
+
+    Other(String),
+}
+
+impl<'a> From<&'a str> for Status {
+    fn from(s: &'a str) -> Self {
+        match s {
+            "current" => Self::Current,
+            "finished" => Self::Finished,
+            "tba" => Self::Tba,
+            "unreleased" => Self::Unreleased,
+            "upcoming" => Self::Upcoming,
+            _ => Self::Other(s.into()),
+        }
+    }
+}
