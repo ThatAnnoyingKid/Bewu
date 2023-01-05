caeb3ccff63eec2fdda4614a02f51e5a8b0511c9
adumbidiot <nathaniel.daniel23@outlook.com>
2023-01-04 22:52:13 -0800

    Auto-generate anime test data

---
+++ lib/kitsu-rs/test_data/anime/generate.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+ids = [
+    13401,
+    46174,
+]
+
+def main():
+    for id in ids:
+        response = requests.get(f'https://kitsu.io/api/edge/anime/{id}')
+        response.raise_for_status()
+        
+        response_json = response.json()
+        with open(f'{id}.json', 'wb') as f:
+            f.write(json.dumps(response_json, indent=4).encode('utf-8'))
+    
+if __name__ == "__main__":
+    main()
+    
