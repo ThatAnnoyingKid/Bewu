a24059b8c8d82a7adaeb0bbed6dbfcc4f5f062c7
adumbidiot <nathaniel.daniel23@outlook.com>
2023-02-17 01:03:33 -0800

    Move media playlist to new mod

--- lib/hls-parser-rs/src/lib.rs
+++ lib/hls-parser-rs/src/lib.rs
@@ -1,5 +1,8 @@
 //! https://datatracker.ietf.org/doc/html/rfc8216
 
+mod media_playlist;
+
+pub use self::media_playlist::MediaPlaylist;
 pub use http::uri::InvalidUri as InvalidUriError;
 pub use http::uri::Uri;
 use std::time::Duration;
@@ -28,10 +31,6 @@ pub enum Error {
         /// The unknown tag
         tag: Box<str>,
     },
-
-    /// Missing colon in `EXT-X-TARGETDURATION` tag
-    #[error("tag \"{EXT_X_TARGET_DURATION_TAG}\" is missing a \":\"")]
-    ExtXTargetDurationTagMissingColon,
 
     /// Invalid EXT_X_TARGET_DURATION_TAG time
     #[error("tag \"{EXT_X_TARGET_DURATION_TAG}\" provided an invalid time")]
@@ -89,146 +88,34 @@ pub enum Error {
         #[source]
         error: std::num::ParseIntError,
     },
-}
 
-/// A media playlist
-#[derive(Debug)]
-pub struct MediaPlaylist {
-    /// The target duration
-    pub target_duration: Duration,
+    /// An attribute value pair is missing an `=`
+    #[error("attribute-value pair missing \"=\"")]
+    AttributeValuePairMissingEquals,
 
-    /// The media segments
-    pub media_segments: Vec<MediaSegment>,
+    /// An attribute was duplicated
+    #[error("duplicate attribute \"{name}\"")]
+    DuplicateAttribute {
+        /// The name of the duplicated attribute
+        name: Box<str>,
+    },
 
-    /// The version
-    pub version: Option<u8>,
+    /// An unknown attribute was supplied
+    #[error("unknown attribute {name}={value}")]
+    UnknownAttributeValuePair {
+        /// The name of the attribute
+        name: Box<str>,
 
-    /// The media sequence number of this first media segment.
-    ///
-    /// If this is `None`, it can be assumed to be 0.
-    pub media_sequence_number: Option<u64>,
-}
-
-impl std::str::FromStr for MediaPlaylist {
-    type Err = Error;
-
-    fn from_str(input: &str) -> Result<Self, Error> {
-        let mut lines = input.lines();
-
-        let start_tag = lines.next().ok_or(Error::UnexpectedEof)?;
-        if start_tag != "#EXTM3U" {
-            return Err(Error::InvalidStartTag {
-                tag: start_tag.into(),
-            });
-        }
-
-        let mut target_duration = None;
-        let mut version = None;
-        let mut media_sequence_number = None;
-
-        let mut ext_inf_tag = None;
-        let mut media_segments = Vec::with_capacity(16);
-        for line in lines {
-            if line.is_empty() {
-                continue;
-            }
-
-            if let Some(line) = line.strip_prefix('#') {
-                if line.starts_with("EXT") {
-                    if let Some(duration) = try_parse_ext_target_duration_tag(line) {
-                        let duration = duration?;
-
-                        if target_duration.is_some() {
-                            return Err(Error::DuplicateTag {
-                                tag: EXT_X_TARGET_DURATION_TAG,
-                            });
-                        }
-
-                        target_duration = Some(duration);
-                    } else if let Some(line) = line.strip_prefix(EXT_INF_TAG) {
-                        let line = line
-                            .strip_prefix(':')
-                            .ok_or(Error::TagMissingColon { tag: EXT_INF_TAG })?;
-                        let (duration, title) =
-                            line.split_once(',').ok_or(Error::ExtInfTagMissingComma)?;
-                        let duration = duration
-                            .parse()
-                            .map(Duration::from_secs_f64)
-                            .map_err(|error| Error::ExtInfTagInvalidDuration { error })?;
-
-                        // Behavior of duped EXTINF tags is unspecified, use the latest one.
-
-                        ext_inf_tag = Some((duration, title))
-                    } else if let Some(line) = line.strip_prefix(EXT_X_VERSION_TAG) {
-                        let line = line.strip_prefix(':').ok_or(Error::TagMissingColon {
-                            tag: EXT_X_VERSION_TAG,
-                        })?;
-                        let parsed: u8 = line
-                            .parse()
-                            .map_err(|error| Error::ExtXVersionTagInvalidVersion { error })?;
-
-                        if version.is_some() {
-                            return Err(Error::DuplicateTag {
-                                tag: EXT_X_VERSION_TAG,
-                            });
-                        }
-
-                        version = Some(parsed);
-                    } else if let Some(line) = line.strip_prefix(EXT_X_MEDIA_SEQUENCE_TAG) {
-                        let line = line.strip_prefix(':').ok_or(Error::TagMissingColon {
-                            tag: EXT_X_MEDIA_SEQUENCE_TAG,
-                        })?;
-                        let parsed: u64 = line.parse().map_err(|error| {
-                            Error::ExtXMediaSequenceTagInvalidSequence { error }
-                        })?;
-
-                        // TODO: Disallow setting after first segment?
-                        // TODO: Disallow dupes?
-                        media_sequence_number = Some(parsed);
-                    } else if let Some(line) = line.strip_prefix(EXT_X_KEY_TAG) {
-                        let _line = line.strip_prefix(':').ok_or(Error::TagMissingColon {
-                            tag: EXT_X_MEDIA_SEQUENCE_TAG,
-                        })?;
-
-                        // TODO: Parse attribute list
-                    } else {
-                        return Err(Error::UnknownTag { tag: line.into() });
-                    }
-                }
-            } else {
-                let uri: Uri = line.parse().map_err(|error| Error::InvalidUri { error })?;
-                let (duration, title) = ext_inf_tag
-                    .take()
-                    .ok_or(Error::MissingTag { tag: EXT_INF_TAG })?;
-
-                media_segments.push(MediaSegment {
-                    duration,
-                    title: title.into(),
-                    uri,
-                })
-            }
-        }
-
-        let target_duration = target_duration.ok_or(Error::MissingTag {
-            tag: EXT_X_TARGET_DURATION_TAG,
-        })?;
-
-        // TODO: Reject if media segment times are higher than target duration?
-
-        Ok(Self {
-            target_duration,
-            media_segments,
-            version,
-            media_sequence_number,
-        })
-    }
+        /// The value of the attribute
+        value: Box<str>,
+    },
 }
 
 fn try_parse_ext_target_duration_tag(line: &str) -> Option<Result<Duration, Error>> {
     line.strip_prefix(EXT_X_TARGET_DURATION_TAG).map(|line| {
-        let line = line
-            .strip_prefix(':')
-            .ok_or(Error::ExtXTargetDurationTagMissingColon)?;
+        let line = line.strip_prefix(':').ok_or(Error::TagMissingColon {
+            tag: EXT_X_TARGET_DURATION_TAG,
+        })?;
         let duration = line
             .parse()
             .map(Duration::from_secs)
@@ -238,90 +125,7 @@ fn try_parse_ext_target_duration_tag(line: &str) -> Option<Result<Duration, Erro
     })
 }
 
-/// A media segment
-#[derive(Debug, PartialEq, Eq)]
-pub struct MediaSegment {
-    /// The duration, in seconds
-    pub duration: Duration,
-
-    /// The title
-    pub title: Box<str>,
-
-    /// The uri
-    pub uri: Uri,
-}
-
 #[cfg(test)]
 mod test {
-    use super::*;
-
-    /// This is provided by the spec.
-    /// Note that it is also invalid,
-    /// as it omits setting the EXT-X-VERSION tag to 3 while using floating point times for EXTINF.
-    ///
-    /// Since the spec disagrees with itself, we choose the more lenient option.
-    const SIMPLE_MEDIA_PLAYLIST: &str = include_str!(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/test_data/simple-media-playlist.m3u8"
-    ));
-
-    const LIVE_MEDIA_PLAYLIST_USING_HTTPS: &str = include_str!(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/test_data/live-media-playlist-using-https.m3u8"
-    ));
-
-    const PLAYLIST_WITH_ENCRYPTED_MEDIA_SEGMENTS: &str = include_str!(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/test_data/playlist-with-encrypted-media-segments.m3u8"
-    ));
-
-    #[test]
-    fn parse_simple_media_playlist() {
-        let playlist: MediaPlaylist = SIMPLE_MEDIA_PLAYLIST.parse().expect("failed to parse");
-        assert!(playlist.target_duration == Duration::from_secs(10));
-        assert!(
-            playlist.media_segments
-                == [
-                    MediaSegment {
-                        duration: Duration::from_secs_f64(9.009),
-                        title: "".into(),
-                        uri: Uri::from_static("http://media.example.com/first.ts"),
-                    },
-                    MediaSegment {
-                        duration: Duration::from_secs_f64(9.009),
-                        title: "".into(),
-                        uri: Uri::from_static("http://media.example.com/second.ts"),
-                    },
-                    MediaSegment {
-                        duration: Duration::from_secs_f64(3.003),
-                        title: "".into(),
-                        uri: Uri::from_static("http://media.example.com/third.ts"),
-                    }
-                ]
-        );
-        assert!(playlist.version.is_none());
-
-        dbg!(&playlist);
-    }
-
-    #[test]
-    fn parse_live_media_playlist_using_https() {
-        let playlist: MediaPlaylist = LIVE_MEDIA_PLAYLIST_USING_HTTPS
-            .parse()
-            .expect("failed to parse");
-        assert!(playlist.version == Some(3));
-        assert!(playlist.media_sequence_number == Some(2680));
-
-        dbg!(&playlist);
-    }
-
-    #[test]
-    fn parse_playlist_with_encrypted_media_segments() {
-        let playlist: MediaPlaylist = PLAYLIST_WITH_ENCRYPTED_MEDIA_SEGMENTS
-            .parse()
-            .expect("failed to parse");
-        assert!(playlist.version == Some(3));
-
-        dbg!(&playlist);
-    }
+    //use super::*;
 }
---
+++ lib/hls-parser-rs/src/media_playlist.rs
@@ -0,0 +1,265 @@
+use crate::try_parse_ext_target_duration_tag;
+use crate::Error;
+use crate::EXT_INF_TAG;
+use crate::EXT_X_KEY_TAG;
+use crate::EXT_X_MEDIA_SEQUENCE_TAG;
+use crate::EXT_X_TARGET_DURATION_TAG;
+use crate::EXT_X_VERSION_TAG;
+use http::Uri;
+use std::time::Duration;
+
+/// A media playlist
+#[derive(Debug)]
+pub struct MediaPlaylist {
+    /// The target duration
+    pub target_duration: Duration,
+
+    /// The media segments
+    pub media_segments: Vec<MediaSegment>,
+
+    /// The version
+    pub version: Option<u8>,
+
+    /// The media sequence number of this first media segment.
+    ///
+    /// If this is `None`, it can be assumed to be 0.
+    pub media_sequence_number: Option<u64>,
+}
+
+impl std::str::FromStr for MediaPlaylist {
+    type Err = Error;
+
+    fn from_str(input: &str) -> Result<Self, Error> {
+        let mut lines = input.lines();
+
+        let start_tag = lines.next().ok_or(Error::UnexpectedEof)?;
+        if start_tag != "#EXTM3U" {
+            return Err(Error::InvalidStartTag {
+                tag: start_tag.into(),
+            });
+        }
+
+        let mut target_duration = None;
+        let mut version = None;
+        let mut media_sequence_number = None;
+
+        let mut ext_inf_tag = None;
+        let mut media_segments = Vec::with_capacity(16);
+        for line in lines {
+            if line.is_empty() {
+                continue;
+            }
+
+            if let Some(line) = line.strip_prefix('#') {
+                if line.starts_with("EXT") {
+                    if let Some(duration) = try_parse_ext_target_duration_tag(line) {
+                        let duration = duration?;
+
+                        if target_duration.is_some() {
+                            return Err(Error::DuplicateTag {
+                                tag: EXT_X_TARGET_DURATION_TAG,
+                            });
+                        }
+
+                        target_duration = Some(duration);
+                    } else if let Some(line) = line.strip_prefix(EXT_INF_TAG) {
+                        let line = line
+                            .strip_prefix(':')
+                            .ok_or(Error::TagMissingColon { tag: EXT_INF_TAG })?;
+                        let (duration, title) =
+                            line.split_once(',').ok_or(Error::ExtInfTagMissingComma)?;
+                        let duration = duration
+                            .parse()
+                            .map(Duration::from_secs_f64)
+                            .map_err(|error| Error::ExtInfTagInvalidDuration { error })?;
+
+                        // Behavior of duped EXTINF tags is unspecified, use the latest one.
+
+                        ext_inf_tag = Some((duration, title))
+                    } else if let Some(line) = line.strip_prefix(EXT_X_VERSION_TAG) {
+                        let line = line.strip_prefix(':').ok_or(Error::TagMissingColon {
+                            tag: EXT_X_VERSION_TAG,
+                        })?;
+                        let parsed: u8 = line
+                            .parse()
+                            .map_err(|error| Error::ExtXVersionTagInvalidVersion { error })?;
+
+                        if version.is_some() {
+                            return Err(Error::DuplicateTag {
+                                tag: EXT_X_VERSION_TAG,
+                            });
+                        }
+
+                        version = Some(parsed);
+                    } else if let Some(line) = line.strip_prefix(EXT_X_MEDIA_SEQUENCE_TAG) {
+                        let line = line.strip_prefix(':').ok_or(Error::TagMissingColon {
+                            tag: EXT_X_MEDIA_SEQUENCE_TAG,
+                        })?;
+                        let parsed: u64 = line.parse().map_err(|error| {
+                            Error::ExtXMediaSequenceTagInvalidSequence { error }
+                        })?;
+
+                        // TODO: Disallow setting after first segment?
+                        // TODO: Disallow dupes?
+                        media_sequence_number = Some(parsed);
+                    } else if let Some(line) = line.strip_prefix(EXT_X_KEY_TAG) {
+                        let line = line.strip_prefix(':').ok_or(Error::TagMissingColon {
+                            tag: EXT_X_MEDIA_SEQUENCE_TAG,
+                        })?;
+
+                        let mut method = None;
+                        let mut uri = None;
+                        for pair in line.split(',') {
+                            let (name, value) = pair
+                                .split_once('=')
+                                .ok_or(Error::AttributeValuePairMissingEquals)?;
+
+                            // TODO: Verify proper attributes are supplied with respect to current attribute state
+                            match name {
+                                "METHOD" => {
+                                    if method.is_some() {
+                                        return Err(Error::DuplicateAttribute {
+                                            name: name.into(),
+                                        });
+                                    }
+
+                                    method = Some(value);
+                                }
+                                "URI" => {
+                                    if uri.is_some() {
+                                        return Err(Error::DuplicateAttribute {
+                                            name: name.into(),
+                                        });
+                                    }
+                                    uri = Some(value);
+                                }
+                                _ => {
+                                    return Err(Error::UnknownAttributeValuePair {
+                                        name: name.into(),
+                                        value: value.into(),
+                                    });
+                                }
+                            }
+                        }
+
+                        // TODO: Apply encryption data to media segments individually
+                    } else {
+                        return Err(Error::UnknownTag { tag: line.into() });
+                    }
+                }
+            } else {
+                let uri: Uri = line.parse().map_err(|error| Error::InvalidUri { error })?;
+                let (duration, title) = ext_inf_tag
+                    .take()
+                    .ok_or(Error::MissingTag { tag: EXT_INF_TAG })?;
+
+                media_segments.push(MediaSegment {
+                    duration,
+                    title: title.into(),
+                    uri,
+                })
+            }
+        }
+
+        let target_duration = target_duration.ok_or(Error::MissingTag {
+            tag: EXT_X_TARGET_DURATION_TAG,
+        })?;
+
+        // TODO: Reject if media segment times are higher than target duration?
+
+        Ok(Self {
+            target_duration,
+            media_segments,
+            version,
+            media_sequence_number,
+        })
+    }
+}
+
+/// A media segment
+#[derive(Debug, PartialEq, Eq)]
+pub struct MediaSegment {
+    /// The duration, in seconds
+    pub duration: Duration,
+
+    /// The title
+    pub title: Box<str>,
+
+    /// The uri
+    pub uri: Uri,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    /// This is provided by the spec.
+    /// Note that it is also invalid,
+    /// as it omits setting the EXT-X-VERSION tag to 3 while using floating point times for EXTINF.
+    ///
+    /// Since the spec disagrees with itself, we choose the more lenient option.
+    const SIMPLE_MEDIA_PLAYLIST: &str = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/test_data/simple-media-playlist.m3u8"
+    ));
+
+    const LIVE_MEDIA_PLAYLIST_USING_HTTPS: &str = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/test_data/live-media-playlist-using-https.m3u8"
+    ));
+
+    const PLAYLIST_WITH_ENCRYPTED_MEDIA_SEGMENTS: &str = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/test_data/playlist-with-encrypted-media-segments.m3u8"
+    ));
+
+    #[test]
+    fn parse_simple_media_playlist() {
+        let playlist: MediaPlaylist = SIMPLE_MEDIA_PLAYLIST.parse().expect("failed to parse");
+        assert!(playlist.target_duration == Duration::from_secs(10));
+        assert!(
+            playlist.media_segments
+                == [
+                    MediaSegment {
+                        duration: Duration::from_secs_f64(9.009),
+                        title: "".into(),
+                        uri: Uri::from_static("http://media.example.com/first.ts"),
+                    },
+                    MediaSegment {
+                        duration: Duration::from_secs_f64(9.009),
+                        title: "".into(),
+                        uri: Uri::from_static("http://media.example.com/second.ts"),
+                    },
+                    MediaSegment {
+                        duration: Duration::from_secs_f64(3.003),
+                        title: "".into(),
+                        uri: Uri::from_static("http://media.example.com/third.ts"),
+                    }
+                ]
+        );
+        assert!(playlist.version.is_none());
+
+        dbg!(&playlist);
+    }
+
+    #[test]
+    fn parse_live_media_playlist_using_https() {
+        let playlist: MediaPlaylist = LIVE_MEDIA_PLAYLIST_USING_HTTPS
+            .parse()
+            .expect("failed to parse");
+        assert!(playlist.version == Some(3));
+        assert!(playlist.media_sequence_number == Some(2680));
+
+        dbg!(&playlist);
+    }
+
+    #[test]
+    fn parse_playlist_with_encrypted_media_segments() {
+        let playlist: MediaPlaylist = PLAYLIST_WITH_ENCRYPTED_MEDIA_SEGMENTS
+            .parse()
+            .expect("failed to parse");
+        assert!(playlist.version == Some(3));
+
+        dbg!(&playlist);
+    }
+}
