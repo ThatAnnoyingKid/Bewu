37e44701e6dd289eea9cb74d07c4dd68d3efc8cb
adumbidiot <nathaniel.daniel23@outlook.com>
2023-03-06 17:44:54 -0800

    Upgrade deps

--- bewu/src/routes.rs
+++ bewu/src/routes.rs
@@ -7,7 +7,6 @@ use axum::http::Request;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::response::Response;
-use axum::routing::get_service;
 use axum::Router;
 use std::sync::Arc;
 use tower_http::services::ServeDir;
@@ -20,11 +19,9 @@ use tower_http::trace::TraceLayer;
 pub fn routes(config: &Config, app_state: Arc<AppState>) -> anyhow::Result<Router> {
     let serve_dir = ServeDir::new(&config.public_directory)
         .not_found_service(tower::service_fn(not_found_error));
-    let serve_dir = get_service(serve_dir).handle_error(server_error);
 
     let data_serve_dir =
         ServeDir::new(&config.data_directory).not_found_service(tower::service_fn(not_found_error));
-    let data_serve_dir = get_service(data_serve_dir).handle_error(server_error);
 
     let trace_layer = TraceLayer::new_for_http()
         .make_span_with(
@@ -44,13 +41,6 @@ pub fn routes(config: &Config, app_state: Arc<AppState>) -> anyhow::Result<Route
     Ok(routes)
 }
 
-async fn not_found_error<T>(_req: Request<T>) -> Result<Response, std::io::Error> {
+async fn not_found_error<T, E>(_req: Request<T>) -> Result<Response, E> {
     Ok((StatusCode::NOT_FOUND, "404: Not Found").into_response())
-}
-
-async fn server_error(_err: std::io::Error) -> impl IntoResponse {
-    (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        "500: Internal Server Error",
-    )
 }
