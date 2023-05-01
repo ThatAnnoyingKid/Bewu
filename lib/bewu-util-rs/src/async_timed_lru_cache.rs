2002e8e95ac5da0371ef5357595bbbe64d5eab6a
adumbidiot <nathaniel.daniel23@outlook.com>
2023-04-30 19:40:35 -0700

    Improve docs

--- lib/bewu-util-rs/src/async_timed_lru_cache.rs
+++ lib/bewu-util-rs/src/async_timed_lru_cache.rs
@@ -5,6 +5,9 @@ use std::time::Duration;
 use std::time::Instant;
 
 /// An async LRU whose entries are valid for only a certian time period.
+///
+/// Entries must impl `Clone` as they are shared when racing and for the `valid_for` Duration.
+/// The simplest way to do this for a non-Clone type is an `Arc`.
 pub struct AsyncTimedLruCache<K, V> {
     cache: std::sync::Mutex<LruCache<K, Arc<tokio::sync::OnceCell<Entry<V>>>>>,
     valid_for: Duration,
@@ -27,13 +30,16 @@ where
         Self { cache, valid_for }
     }
 
+    /// Get an entry by a value, providing a function that returns a future that is called if the cache entry is expired or empty.
+    ///
+    /// It is guaranteed that the function will be called only once if many gets run in parallel.
     pub async fn get<FN, FUT>(&self, key: K, func: FN) -> V
     where
         FN: FnOnce() -> FUT,
         FUT: Future<Output = V>,
     {
         let entry = {
-            let mut cache = self.cache.lock().unwrap();
+            let mut cache = self.cache.lock().expect("cache poisoned");
 
             match cache.get_mut(&key) {
                 Some(entry) => match entry.get() {
@@ -72,15 +78,25 @@ where
     }
 }
 
-impl<K, V> std::fmt::Debug for AsyncTimedLruCache<K, V> {
+impl<K, V> std::fmt::Debug for AsyncTimedLruCache<K, V>
+where
+    K: std::hash::Hash + std::cmp::Eq,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // TODO: Provide more info
-        f.debug_struct("AsyncTimedLruCache").finish()
+        f.debug_struct("AsyncTimedLruCache")
+            .field("cache", &self.cache)
+            .field("valid_for", &self.valid_for)
+            .finish()
     }
 }
 
+/// A cache entry
 #[derive(Debug)]
 struct Entry<V> {
+    /// The time this entry was created.
     created: Instant,
+
+    /// The value of the entry.
     value: V,
 }
